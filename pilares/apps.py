3d4a29ca18e3c73c654691ec17b6ab127ba786b1
Erick25edc <erick.garcia@pilares.cdmx.gob.mx>
2026-06-21 19:29:57 -0600

    Update apps.py

--- pilares/apps.py
+++ pilares/apps.py
@@ -2,4 +2,8 @@ from django.apps import AppConfig
 
 
 class PilaresConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'pilares'
+    
+    def ready(self):
+        import pilares.signals  # ← Agrega esta línea
